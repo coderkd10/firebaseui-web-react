390419559f2549c7b98a2354b923d2f30dc3613d
Abhishek Kedia <kedia.abhishek10@gmail.com>
2023-11-23 10:37:01 +0000

    cleanup

--- scripts/serve-example.sh
+++ scripts/serve-example.sh
@@ -6,3 +6,6 @@ docker run -it --rm --publish 8000:8000 --name cont-example-1  rfb-example:build
 # to ssh into the server run the following in a different terminal
 # docker exec -it cont-example-1 /bin/bash
 # [sudo] docker exec -u root -it cont-example-1 /bin/bash
+
+# pull the built static bundle using
+# docker cp cont-example-1:/home/node/app/example/public example/
