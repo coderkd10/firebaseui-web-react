1465390618e513129020fae4530fcd9a2fc4acf9
Abhishek Kedia <kedia.abhishek10@gmail.com>
2023-11-29 09:29:57 +0000

    fix object rest spread

--- example/src/MyFirebaseAuth.jsx
+++ example/src/MyFirebaseAuth.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 
+function _objectWithoutProperties(srcObj, excluded) {
+    // hack since we don't have object rest spread
+    // adapted from @babel/plugin-transform-object-rest-spread
+    const out = {};
+    for (let key of Object.keys(srcObj)) {
+        if (excluded.indexOf(key) === -1) {
+            out[key] = srcObj[key];
+        }
+    }
+    return out;
+}
+
 function MyFbAuthProxy(props) {
     // for some reason this syntax isn't working!
     // const { useStyled, ...downstreamProps } = props;
 
     const useStyled = props.useStyled;
-    const downstreamProps = props;
-    delete downstreamProps['useStyled'];
+    const downstreamProps = _objectWithoutProperties(props, ['useStyled']);
 
     if (useStyled) {
         const FirebaseAuth = require('react-firebaseui/FirebaseAuth').default;
