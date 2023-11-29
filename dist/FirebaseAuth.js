ded99e9dbfc8a1fbd9ec0d5510d00576da83da4f
Abhishek Kedia <kedia.abhishek10@gmail.com>
2023-11-29 08:04:21 +0000

    reset dist, src to upstream:master

--- dist/FirebaseAuth.js
+++ dist/FirebaseAuth.js
@@ -1,24 +1,84 @@
-"use strict";
+'use strict';
 
 exports.__esModule = true;
-exports.AnotherComp = AnotherComp;
-exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-var FirebaseAuth = exports.default = function (_React$Component) {
-  _inheritsLoose(FirebaseAuth, _React$Component);
-  function FirebaseAuth() {
-    return _React$Component.apply(this, arguments) || this;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ELEMENT_ID = 'firebaseui_container';
+
+var firebaseUiDeletion = Promise.resolve();
+
+var FirebaseAuth = function (_React$Component) {
+  _inherits(FirebaseAuth, _React$Component);
+
+  function FirebaseAuth(props) {
+    _classCallCheck(this, FirebaseAuth);
+
+    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
+
+    _this.uiConfig = props.uiConfig;
+    _this.firebaseAuth = props.firebaseAuth;
+    _this.className = props.className;
+    _this.uiCallback = props.uiCallback;
+    _this.unregisterAuthObserver = function () {};
+    return _this;
   }
-  var _proto = FirebaseAuth.prototype;
-  _proto.render = function render() {
-    return _react.default.createElement("div", null, _react.default.createElement("h1", null, "THIS IS SAMPLE FirebaseAuth component"));
+
+  FirebaseAuth.prototype.componentDidMount = function componentDidMount() {
+    var _this2 = this;
+
+    require('firebaseui/dist/firebaseui.css');
+
+    var firebaseui = require('firebaseui');
+
+    return firebaseUiDeletion.then(function () {
+      _this2.firebaseUiWidget = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(_this2.firebaseAuth);
+      if (_this2.uiConfig.signInFlow === 'popup') {
+        _this2.firebaseUiWidget.reset();
+      }
+
+      _this2.userSignedIn = false;
+      _this2.unregisterAuthObserver = _this2.firebaseAuth.onAuthStateChanged(function (user) {
+        if (!user && _this2.userSignedIn) {
+          _this2.firebaseUiWidget.reset();
+        }
+        _this2.userSignedIn = !!user;
+      });
+
+      if (_this2.uiCallback) {
+        _this2.uiCallback(_this2.firebaseUiWidget);
+      }
+
+      _this2.firebaseUiWidget.start('#' + ELEMENT_ID, _this2.uiConfig);
+    });
   };
+
+  FirebaseAuth.prototype.componentWillUnmount = function componentWillUnmount() {
+    var _this3 = this;
+
+    firebaseUiDeletion = firebaseUiDeletion.then(function () {
+      _this3.unregisterAuthObserver();
+      return _this3.firebaseUiWidget.delete();
+    });
+    return firebaseUiDeletion;
+  };
+
+  FirebaseAuth.prototype.render = function render() {
+    return _react2.default.createElement('div', { className: this.className, id: ELEMENT_ID });
+  };
+
   return FirebaseAuth;
-}(_react.default.Component);
-function AnotherComp() {
-  return _react.default.createElement("h1", null, "THIS IS A SIMPLE FUNCTIONAL COMPONENT");
-}
+}(_react2.default.Component);
+
+exports.default = FirebaseAuth;
 //# sourceMappingURL=FirebaseAuth.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,15 @@
-"use strict";
+'use strict';
 
-var _FirebaseAuth = _interopRequireDefault(require("./FirebaseAuth"));
-var _StyledFirebaseAuth = _interopRequireDefault(require("./StyledFirebaseAuth"));
+var _FirebaseAuth = require('./FirebaseAuth');
+
+var _FirebaseAuth2 = _interopRequireDefault(_FirebaseAuth);
+
+var _StyledFirebaseAuth = require('./StyledFirebaseAuth');
+
+var _StyledFirebaseAuth2 = _interopRequireDefault(_StyledFirebaseAuth);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-exports.FirebaseAuth = _FirebaseAuth.default;
-exports.StyledFirebaseAuth = _StyledFirebaseAuth.default;
+
+exports.FirebaseAuth = _FirebaseAuth2.default;
+exports.StyledFirebaseAuth = _StyledFirebaseAuth2.default;
 //# sourceMappingURL=index.js.map
